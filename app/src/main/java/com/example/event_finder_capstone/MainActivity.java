6f135f1d97821ba1c985418041e7982909674f9c
Michelle Ly <michellely@fb.com>
2022-06-20 11:18:18 -0700

    Initial commit

---
+++ app/src/main/java/com/example/event_finder_capstone/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.event_finder_capstone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
