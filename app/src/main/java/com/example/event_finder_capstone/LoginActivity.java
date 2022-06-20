7bc760ea9a31ee1548daeb465545d0e96d351bf2
Michelle Ly <michellely@fb.com>
2022-06-20 15:09:40 -0700

    Created sign up screen and connected sign up button to corresponding screen

--- app/src/main/java/com/example/event_finder_capstone/LoginActivity.java
+++ app/src/main/java/com/example/event_finder_capstone/LoginActivity.java
@@ -1,6 +1,59 @@
 package com.example.event_finder_capstone;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
 public class LoginActivity extends AppCompatActivity {
+
+    public static final String TAG = "LoginActivity";
+    private EditText etUsername;
+    private EditText etPassword;
+    private Button btnLogin;
+    private Button btnSignUp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnSignUp = findViewById(R.id.btnSignUp);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginUser();
+            }
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signupUser();
+            }
+        });
+    }
+
+    private void signupUser() {
+        Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+        startActivity(intent);
+    }
+
+    private void loginUser() {
+        Toast.makeText(LoginActivity.this, "Login Test!", Toast.LENGTH_LONG).show();
+        return;
+    }
 }
