71dabe5f028348dcb7e4787e96f6be7078f9ea1d
Michelle Ly <michellely@fb.com>
2022-06-27 16:49:33 -0700

    Added image, description, submit/upload photo button, and progress bar to activity_add_photo.xml

--- app/src/main/java/com/example/event_finder_capstone/PhotoAlbumActivity.java
+++ app/src/main/java/com/example/event_finder_capstone/PhotoAlbumActivity.java
@@ -4,14 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.parse.ParseUser;
 
 public class PhotoAlbumActivity extends AppCompatActivity {
 
