af34a26612af70b0c0e801cd3ec7a01302b02ac2
Michelle Ly <michellely@fb.com>
2022-07-06 11:14:29 -0700

    Added EventViewModel to manage data and link view and the event model

---
+++ app/src/main/java/com/capstone/event_finder/network/EventViewModel.java
@@ -0,0 +1,31 @@
+package com.capstone.event_finder.network;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.capstone.event_finder.models.Event;
+
+import java.util.List;
+
+public class EventViewModel extends AndroidViewModel {
+
+    public LiveData<List<Event>> getEvents;
+    private EventRepository eventRepository;
+
+    public EventViewModel(@NonNull Application application) {
+        super(application);
+        eventRepository = new EventRepository(application);
+        getEvents = eventRepository.getEvents();
+    }
+
+    public void insert(List<Event> events) {
+        eventRepository.insert(events);
+    }
+
+    public LiveData<List<Event>> getAllCats() {
+        return getEvents;
+    }
+}
