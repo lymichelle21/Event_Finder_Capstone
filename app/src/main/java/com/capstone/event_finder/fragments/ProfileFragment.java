1d034a6814be9a6ac062d635effdfa67a010811c
Michelle Ly <michellely@fb.com>
2022-07-08 10:31:11 -0700

    Added username and bookmarks RecyclerView to profile fragment xml, display username on profile fragment

--- app/src/main/java/com/capstone/event_finder/fragments/ProfileFragment.java
+++ app/src/main/java/com/capstone/event_finder/fragments/ProfileFragment.java
@@ -4,13 +4,19 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import com.capstone.event_finder.R;
+import com.parse.ParseUser;
+
+import org.w3c.dom.Text;
 
 public class ProfileFragment extends Fragment {
+
+    TextView tvProfileUsername;
 
     public ProfileFragment() {
     }
@@ -24,6 +30,9 @@ public class ProfileFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        tvProfileUsername = view.findViewById(R.id.tvProfileUsername);
+        tvProfileUsername.setText(ParseUser.getCurrentUser().getUsername());
     }
 
 }
